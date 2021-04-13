beeb11cc72a97cfe902095d7548d021886f11cf1
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-04-12 17:58:59 -0700

    bookapi 20210412 1558

---
+++ CS321_W3D1_BookAPI/CS321_W3D1_BookAPI/Data/BookContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CS321_W3D1_BookAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CS321_W3D1_BookAPI.Data
+{
+    public class BookContext : DbContext
+    {
+        // TODO: implement a DbSet<Book> property
+
+        public BookContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        // This method runs once when the DbContext is first used.
+        // It's a place where we can customize how EF Core maps our
+        // model to the database. 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO: configure some seed data in the books table
+            
+            base.OnModelCreating(modelBuilder);
+            
+        }
+
+    }
+}
+
